7f87561d1e2b3b8e8b6e313062c3a49bbe87571c
nicolasmanic <nicolas.kanakis@gmail.com>
2017-07-28 00:45:47 +0300

    minor fixes for better testability

--- src/main/java/gr/personal/aggregator/AggregatorExecutor.java
+++ src/main/java/gr/personal/aggregator/AggregatorExecutor.java
@@ -3,6 +3,9 @@ package gr.personal.aggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,9 +16,14 @@ import java.util.concurrent.Executors;
 public class AggregatorExecutor {
     @Autowired
     private AggregatorRunnable aggregator;
+    private ExecutorService executorService;
 
-    public void aggregate(){
+    public void start(){
         ExecutorService executorService = Executors.newFixedThreadPool(1);
         executorService.execute(aggregator);
+    }
+
+    public void stop() {
+        executorService.shutdown();
     }
 }
--- src/test/java/gr/personal/aggregator/AggregatorExecutorTest.java
+++ src/test/java/gr/personal/aggregator/AggregatorExecutorTest.java
@@ -1,11 +1,19 @@
 package gr.personal.aggregator;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by Nick Kanakis on 25/7/2017.
@@ -17,8 +25,18 @@ public class AggregatorExecutorTest {
     @Autowired
     private AggregatorExecutor aggregatorExecutor;
 
-    @Test
+    @Before
+    public void setUp() throws Exception {
+        aggregatorExecutor.start();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        aggregatorExecutor.stop();
+    }
+
+    @Test(timeout = 30000)
     public void testAggregate() throws Exception {
-        //todo: create test case
+        //todo add test case
     }
 }
