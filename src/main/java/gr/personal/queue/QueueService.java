900b3fcf814e0f40a1e11b58092d49c811716883
nkanakis <nicolas.kanakis@gmail.com>
2017-08-10 15:15:07 +0300

    Remove useless code

--- src/main/java/gr/personal/queue/QueueService.java
+++ src/main/java/gr/personal/queue/QueueService.java
@@ -1,12 +1,14 @@
 package gr.personal.queue;
 
-import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
 
 /**
  * Created by nkanakis on 8/9/2017.
  */
 public interface QueueService {
-    void enqueueComment(JSONArray result);
+    void enqueueComment(JSONObject model2Enqueue);
 
-    void enqueuePost(JSONArray result);
+    void enqueuePost(JSONObject model2Enqueue);
 }
